35d9022b8a77d7b57d731014a344cec66483de58
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-16 17:19:59 +0700

    s

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  //Long Trong The Div va goi trong app.component.html
-  // selector : '[app-server]',
-  selector : '.app-server.',
-  templateUrl:'./server.component.html'
+  // Long Trong The Div va goi trong app.component.html
+  selector: 'app-server',
+  // selector : '.app-server.',
+  templateUrl: './server.component.html'
 })
-export class ServerComponent{
-
+export class ServerComponent {
+ServerID = 10;
+ServerStatus = 'offline';
+getServerStatus() {
+  return this.ServerStatus;
+}
 }
